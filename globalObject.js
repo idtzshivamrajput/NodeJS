5182f3f67a5f529e4047d6bee682217b39f4ded8
idtzshivamrajput <113540823+idtzshivamrajput@users.noreply.github.com>
2022-11-10 22:07:14 +0530

    87

--- globalObject.js
+++ globalObject.js
@@ -2,9 +2,10 @@
 // __filename
 // module
 // exports
-
 // console.log(__dirname);
 // console.log(__filename);
+
 const data=require('./student');
+
 console.log(data.name);
 console.log(data.name1);
